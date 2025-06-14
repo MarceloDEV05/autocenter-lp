c9da1ed4383d6ae3745c310235c1ea923359d0e0
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-06-13 21:20:17 -0300

    adicionando links nos icones

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -9,19 +9,19 @@ export const Contacts = () => {
 
             <aside className=" w-full rounded-md h-12 flex items-center justify-center border-white bg-yellow-400 hover:scale-105 duration-300">
             <a href="https://wa.me/55016992611903">
-            <FaWhatsapp size={40}/>
+            <FaWhatsapp size={40} target="_blank"/>
             </a>
             </aside>
 
             <aside className=" w-full h-12 rounded-md flex items-center justify-center border-white bg-yellow-400 hover:scale-105 duration-300">
-            <a href="">
+            <a target="_blank" href="https://www.instagram.com/veto.pereira?igsh=N2ttNmhsd2s1ZGV2">
             <FaInstagram size={40}/>
             </a>
             </aside>
 
 
             <aside className="w-full h-12 rounded-md flex items-center justify-center border-white bg-yellow-400 hover:scale-105 duration-300">
-            <a href="">
+            <a href="mailto:vetodeoliveira@gmail.com?subject=Contato via site&body=Olá, gostaria de mais informações sobre..." target="_blank">
             <FaEnvelope size={40}/>
             </a>
             </aside>
