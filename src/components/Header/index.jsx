1b5e8e38bdf958c1173536f8d7f17d22bd4bdd7e
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-06-16 02:45:54 -0300

    atualizando logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,10 @@
+import logo from '../../assets/image.png'
 export const Header = () => {
     return(
         <header className="bg-gray-950 w-full h-16 items-center justify-around px-8 rounded-lg flex z-50 fixed top-0 text-white gap-2 overflow-hidden">
-            <h1 className="text-white lg:text-3xl text-xl text-center">Erivelton <strong className="text-yellow-400">AutoCenter</strong></h1>
+            <h1>
+                <img src={logo} alt="" className='w-35' />
+            </h1>
 
             <nav className="z-50 hidden lg:block">
                 <ul className="flex gap-3 text-lg">
--- src/components/Interface/index.jsx
+++ src/components/Interface/index.jsx
@@ -1,7 +1,8 @@
 
-import img from '../../assets/fundo-de-exibicao-grunge-escuro-3d-com-atmosfera-esfumacada_1048-16218.avif'
+import img from '../../assets/background.avif'
 
-import carImg from "../../assets/volkswagen-gol-1.0-2023-removebg-preview.png"
+
+import carImg from "../../assets/car-interface.png"
 import { FaWhatsapp } from 'react-icons/fa6'
 import { motion } from 'framer-motion'
 
@@ -17,7 +18,7 @@ export const Interface = () => {
         className='flex justify-center bg-cover bg-center h-screen object-cover w-full' style={{backgroundImage: `url(${img})`}} id='home'>
            
            
-          <article className='flex items-center justify-around w-full'>
+          <article className='flex items-center justify-around w-full max-w-7xl'>
 
                  <section className='hidden lg:block md:block w-full  '>
                     <img src={carImg} alt=""/>
@@ -25,10 +26,10 @@ export const Interface = () => {
 
                 <div className=''>
 
-                <h1 className='text-white justify-center lg:text-3xl text-[24px] flex gap-1 px-4'>Erivelton <br/>
+                <h1 className='text-white lg:text-3xl text-[24px] flex gap-1 px-4'>Erivelton <br/>
                 <strong className='text-yellow-400 lg:text-3xl text-[24px]'>AutoCenter</strong>
                 </h1>
-                <p className="text-white font-medium text-[17px] lg:w-full lg:max-w-300 w-full max-w-90 px-4">Seu carro merece cuidado de verdade. Aqui na Erivelton Auto Center, fazemos alinhamento, balanceamento, troca de óleo, revisão completa e muito mais, sempre com atenção aos detalhes e preço justo.<br/>
+                <p className="text-white font-medium text-[17px] lg:w-full lg:max-w-200 w-full max-w-90 px-4">Seu carro merece cuidado de verdade. Aqui na Erivelton Auto Center, fazemos alinhamento, balanceamento, troca de óleo, revisão completa e muito mais, sempre com atenção aos detalhes e preço justo.<br/>
 
                 <strong className='font-bold'>👉 Agende seu serviço e dirija tranquilo!</strong>
                 </p>
