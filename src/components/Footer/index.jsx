78f3ff3956ce80e008e36782be50e30188bde5de
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2025-06-24 13:03:41 -0300

    mudando copy

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
     return(
         <footer className="bg-gray-950 flex items-center justify-center w-full bottom-0 h-10 top-full">
-           <p className="text-gray-500">&copy; 2025 MarceloCardoso.Dev</p>
+           <p className="text-gray-500">&copy; 2025 Erivelton Auto Center</p>
         </footer>
     )
 }
